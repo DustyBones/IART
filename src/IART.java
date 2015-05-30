68c57d6f4aa529a71036d5b7b8f5c9b471524c8d
DustyBones <tiago.fa.figueiredo@gmail.com>
2015-05-30 07:50:51 +0100

    load data, modify location to a single value

---
+++ src/IART.java
@@ -0,0 +1,15 @@
+public class IART {
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length != 0) {
+            System.out.println("No arguments expected");
+            return;
+        }
+
+        DataSet dataSet = new DataSet("default_features_1059_tracks.txt");
+
+        dataSet.setUp();
+
+    }
+}
